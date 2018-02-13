5c8c4530a669b37dc5dd32dfb5e979dcde8a4a34
Blake Mealey <blakemealey@gmail.com>
2018-02-12 17:03:29 -0700

    Bloom effect setup

--- Engine/Engine/Engine/Systems/Graphics.h
+++ Engine/Engine/Engine/Systems/Graphics.h
@@ -31,7 +31,7 @@ struct Camera {
 };
 
 struct VAOs {
-	enum { Geometry=0, ShadowMap, Skybox, Count };
+	enum { Geometry=0, ShadowMap, Skybox, Screen, Count };
 };
 
 struct VBOs {
@@ -43,15 +43,19 @@ struct SSBOs {
 };
 
 struct FBOs {
-	enum { ShadowMap=0, Count };
+	enum { ShadowMap=0, Screen, Count };
+};
+
+struct RBOs {
+    enum { Depth=0, Count };
 };
 
 struct Textures {
-	enum { ShadowMap=0, Count };
+	enum { ShadowMap=0, Screen, ScreenGlow, Count };
 };
 
 struct Shaders {
-	enum { Geometry=0, ShadowMap, Skybox, Count };
+	enum { Geometry=0, ShadowMap, Skybox, Screen, Count };
 };
 
 class Graphics : public System {
@@ -66,6 +70,8 @@ public:
 	static const std::string SHADOW_MAP_FRAGMENT_SHADER;
     static const std::string SKYBOX_VERTEX_SHADER;
     static const std::string SKYBOX_FRAGMENT_SHADER;
+    static const std::string SCREEN_VERTEX_SHADER;
+    static const std::string SCREEN_FRAGMENT_SHADER;
 
 	static const size_t MAX_CAMERAS;
 
@@ -97,6 +103,8 @@ private:
 	Graphics(const Graphics&) = delete;
 	Graphics& operator= (const Graphics&) = delete;
 
+    ~Graphics();
+
 	void LoadModel(ShaderProgram* shaderProgram, MeshComponent* model);
 
 	void LoadCameras(std::vector<Component*> cameraComponents);
@@ -112,6 +120,7 @@ private:
 	GLuint vaoIds[VAOs::Count];
 	GLuint ssboIds[SSBOs::Count];
 	GLuint fboIds[FBOs::Count];
+	GLuint rboIds[RBOs::Count];
 	GLuint textureIds[Textures::Count];
 	ShaderProgram* shaders[Shaders::Count];
 
@@ -126,13 +135,15 @@ private:
 	void LoadUvs(const glm::vec2 *uvs, const size_t vertexCount);
 	void LoadNormals(const glm::vec3 *normals, const size_t vertexCount);
 
-	void DestroyIds();
+	void DestroyIds() const;
 	void GenerateIds();
 
 	void InitializeGeometryVao();
 	void InitializeShadowMapVao();
 	void InitializeSkyboxVao();
+	void InitializeScreenVao();
 
+	void InitializeScreenFramebuffer();
 	void InitializeShadowMapFramebuffer();
 	ShaderProgram* LoadShaderProgram(std::string vertexShaderFile, std::string fragmentShaderFile) const;
 };
